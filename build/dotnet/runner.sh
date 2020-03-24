f70043dcd116ec1c568633c2def3b542fe717185
strebulaev <strebulaev@gmail.com>
2020-03-24 07:49:57 +0300

    dotnet runner: bug fixes

--- build/dotnet/runner.sh
+++ build/dotnet/runner.sh
@@ -4,7 +4,7 @@ file=/registered
 
 if [ ! -f $file ]; then
     touch $file
-    gitlab-runner register -n
+    gitlab-runner register -n -c /home/gitlab-runner/config.toml
 fi
 
 /usr/bin/dumb-init /entrypoint run --user=gitlab-runner --working-directory=/home/gitlab-runner
